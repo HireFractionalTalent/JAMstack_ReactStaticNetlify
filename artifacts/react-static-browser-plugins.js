6266ec8cdefc3ffaec9446911ce5f343be9b33ab
sean@freemiummedia.com <sean@freemiummedia.com>
2020-01-21 00:58:54 -0500

    all

---
+++ artifacts/react-static-browser-plugins.js
@@ -0,0 +1,27 @@
+// Imports
+import plugin0 from 'C:/Users/freemiummedia-001/training/JAMstack_ReactStaticNetlify/node_modules/react-static-plugin-reach-router/browser.api.js'
+
+// Plugins
+const plugins = [{
+        location: "C:/Users/freemiummedia-001/training/JAMstack_ReactStaticNetlify/node_modules/react-static-plugin-source-filesystem",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "C:/Users/freemiummedia-001/training/JAMstack_ReactStaticNetlify/node_modules/react-static-plugin-reach-router",
+        plugins: [],
+        hooks: plugin0({})
+      },
+{
+        location: "C:/Users/freemiummedia-001/training/JAMstack_ReactStaticNetlify/node_modules/react-static-plugin-sitemap/dist",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "C:/Users/freemiummedia-001/training/JAMstack_ReactStaticNetlify",
+        plugins: [],
+        hooks: {}
+      }]
+
+// Export em!
+export default plugins
---
+++ artifacts/react-static-templates.js
@@ -0,0 +1,14 @@
+
+
+  
+// Template Map
+export default {
+  'C:/Users/freemiummedia-001/training/JAMstack_ReactStaticNetlify/src/pages/404.js': require('C:/Users/freemiummedia-001/training/JAMstack_ReactStaticNetlify/src/pages/404.js').default,
+'C:/Users/freemiummedia-001/training/JAMstack_ReactStaticNetlify/src/pages/about.js': require('C:/Users/freemiummedia-001/training/JAMstack_ReactStaticNetlify/src/pages/about.js').default,
+'C:/Users/freemiummedia-001/training/JAMstack_ReactStaticNetlify/src/pages/blog.js': require('C:/Users/freemiummedia-001/training/JAMstack_ReactStaticNetlify/src/pages/blog.js').default,
+'C:/Users/freemiummedia-001/training/JAMstack_ReactStaticNetlify/src/pages/index.js': require('C:/Users/freemiummedia-001/training/JAMstack_ReactStaticNetlify/src/pages/index.js').default,
+'C:/Users/freemiummedia-001/training/JAMstack_ReactStaticNetlify/src/containers/Post': require('C:/Users/freemiummedia-001/training/JAMstack_ReactStaticNetlify/src/containers/Post').default
+}
+
+export const notFoundTemplate = 'C:/Users/freemiummedia-001/training/JAMstack_ReactStaticNetlify/src/pages/404.js'
+
